18957c30858ceba5b470acee5e6c364a3d1150ee
Sanju Mondal <sanjumondal191202@gmail.com>
2023-07-11 10:01:12 +0530

    added index3.js and changes made in index2.js

--- letsLearnGit/index2.js
+++ letsLearnGit/index2.js
@@ -2,4 +2,4 @@ let a = 30;
 let b = 20;
 let diff = a - b;
 
-console.log("sum of a and b is: "+diff);
+console.log("Difference of a and b is: "+diff);
---
+++ letsLearnGit/index3.js
@@ -0,0 +1,10 @@
+let a = prompt("Enter Your Age");
+a = Number.parseInt(a);
+if(a > 0)
+{
+	alert("this is a valid age");
+}
+else
+{
+	alert("this is an invalid age");
+}
