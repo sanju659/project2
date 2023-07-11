238a356fef98c3a06cae8c81ba4d1d71a7f37394
Sanju Mondal <sanjumondal191202@gmail.com>
2023-07-11 09:53:04 +0530

    Initial Commit

---
+++ letsLearnGit/index1.js
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 30;
+let sum = a + b;
+
+console.log("sum of a and b is: "+sum);
---
+++ letsLearnGit/index2.js
@@ -0,0 +1,5 @@
+let a = 30;
+let b = 20;
+let diff = a - b;
+
+console.log("sum of a and b is: "+diff);
